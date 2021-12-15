3be389fdd8b1772c59356aea1731beeb7924be50
Jules-Latapy <76938929+Jules-Latapy@users.noreply.github.com>
2021-12-15 20:11:19 +0100

    Add files via upload

---
+++ Dico.js
@@ -0,0 +1,70 @@
+	/*--------------------Compilation/lancement-----------*/
+		//node Dico.js
+	/*--------------------Types---------------------------*/
+		//typage implicite
+		//=>Object, Number, String, Boolean, Null et Undefined.
+		var int = 0 ;
+		var char = 'o';
+	/*--------------------Tableau et collection-----------*/
+		var tab = [1,2,3];
+		tab = new Array("un","deux","trois");
+	/*--------------------expression----------------------*/
+	/*
+		||=ou
+		&&=et
+		^ =xou
+		!= inegalité
+		== egalité
+		>= supOuEgal
+		<= infOuEgal
+		+ concatenation
+		=== comparaison
+	*/
+	/*--------------------if------------------------------*/
+		if (true) {
+
+		} else {
+
+		}
+
+		var i =(true?"vraie":"faux");
+
+		switch(i){
+			case "vraie":  i="afficionado";  break; 
+			default     :  i="None"       ;  
+		}
+	/*--------------------boucles-------------------------*/
+		while(false){
+
+		}
+
+		do{
+
+		}while(false);
+
+		for (var i = tab.length - 1; i >= 0; i--) {
+
+		}
+
+	/*--------------------function/sous programme---------*/
+		function function_name(argument) {
+			// body...
+		}
+	/*--------------------package-------------------------*/
+
+	/*--------------------Exception-----------------------*/
+		throw 'Parameter is not a number!';
+		try{}catch(error){}
+	/*--------------------Multithread/task----------------*/
+
+	/*----------------------------------------------------*/
+	/*                    Objet                           */
+	/*----------------------------------------------------*/
+
+	/*--------------------heritage------------------------*/
+
+	/*--------------------mot de language-----------------*/
+
+/*
+notabene javascript ce sert des objet sans classe (on ne peut donc pas crée le meme objet)
+*/
