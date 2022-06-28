9a671d1fc65025d84b87db8d5a1c88adf9cbcf8e
Jules <jullatapy@gmail.com>
2022-06-28 23:22:06 +0200

    update

--- Dico.c
+++ Dico.c
@@ -2,7 +2,9 @@
 		gcc leProgramme.c -o execuble.x
 		./execuble.x
 	//--------------------Types---------------------------//
-		boolean b ;
+		unsigned 
+		signed
+		auto	//instruction de création de variable(on force a crée)
 
 		char
 
@@ -16,20 +18,40 @@
 
 		double
 
-		*int
+		*int pointeur 
+		&addresse
 	//--------------------Tableau et collection-----------//
 		char* s = (char*) malloc (12*sizeof(char*));
 		*s[0] = s ;
+
+		struct aggrega
+		{
+			//aggrega de types
+		};
+
+		union aggrega2{
+			//aggrega de types mais qui partage la meme memoire 
+			//chacun peut avoir une valeur a la fois
+		};
+
+		enum enumeration {
+			LUNDI,
+			MARDI=2,
+			MERCREDI,
+			JEUDI,
+			VENDREDI,
+			SAMEDI,
+			DIMANCHE;
+		}
 	//--------------------expression----------------------//
-		/*
-		||=ou
-		&&=et
-		^ =xou
+
+		|| ou
+		&& et
+		^  xou
 		!= inegalité
 		== egalité
 		>= supOuEgal
 		<= infOuEgal
-		*/
 	//--------------------if------------------------------//
 		if (/* condition */)
 		{
@@ -42,44 +64,53 @@
 
 		switch(){
 			case : break ;
+			default : ;
 		}
 	//--------------------boucles-------------------------//
-		while(){}
+		while(){
+			/* code */
+		}
 
-		do{}while();
+		do{
+			/* code */
+		}while();
+
+		for (int i = 0; i < count; ++i)
+		{
+			/* code */
+		}
 	//--------------------function/sous programme---------//
 		int function()
 		{return 3;}
 
-		void procedure()
-		{}
-
-		struct truc(int arg)
-		{
-			//aggrega de trucs
-		};
 	//--------------------package-------------------------//
 		//renommer le fichier en .h
 		#import <stdio.h> ;
+		extern
 	//--------------------Exception-----------------------//
 
 	//--------------------Multithread/task----------------//
 		int pid=fork();
-		wait(NULL);//point de rendez vous
-	//----------------------------------------------------//
-	//                    Objet                           //
-	//----------------------------------------------------//
-
-	//--------------------heritage------------------------//
+		wait(NULL);    //point de rendez vous
 
 	//--------------------mot de language-----------------//
 
-		printf("essaie\n", );
-
-		int main(int argc, char const *argv[])
-		{
-			/* code */
-			return 0;
-		}
-
-		assert(true);
+		
+	break	
+	case	
+	const	
+	continue
+	default	
+	do
+	else	
+	extern		//dans un autre fichier
+	for	
+	goto	
+	if	
+	register	//la variable va dans le CPU plutot que la memoire /!\ on ne peut acceder a l'addresse et elle doit etre local
+	return
+	sizeof	
+	static		//variables persistante jusqu'a la fin du programme
+	switch	
+	volatile	//empeche l'optimisation du code de la variable
+	while
--- Dico.php
+++ Dico.php
@@ -1,115 +1,44 @@
 <?php
 	/*-------------------Compilation/lancement----------*/
-		//?
+
 	/*-------------------Types--------------------------*/
-		$type = 0 ;
+
 	/*-------------------Tableau et collection----------*/
-		$arrayName = array('clef' => valeur, );
+
 	/*-------------------expression---------------------*/
-		||=ou
-		&&=et
-		^ =xou
-		!= inegalité
-		== egalité
-		>= supOuEgal
-		<= infOuEgal
-		. concatenation
+
 	/*-------------------if-----------------------------*/
-		if (condition) {
-			// code...
-		}elseif (condition) {
-			// code...
-		}else{
 
-		}
-
-		$var = (bool)?"oui":"non";
-
-		switch (variable) {
-			case 'value':
-				// code...
-				break;
-			
-			default:
-				// code...
-				break;
-		}
 	/*-------------------boucles------------------------*/
 
-		while (condition) {
-			// code...
-		}
-
-		do{
-
-		}
-		while()
-
-		for ($i=0; $i < $value; $i++) { 
-			// code...
-		}
-
-		foreach ($variable as $key => $value) {
-			// code...
-		}
 	/*-------------------function/sous programme--------*/
-		function FunctionName($value='')
-		{
-			//peut retourner nimporte quoi
-		}
+
 	/*-------------------package------------------------*/
-		--> renommer sont fichier en ".inc.php"
+
 	/*-------------------Exception----------------------*/
-		try {
-			
-		} catch (Exception $e) {
-			
-		}
+
 	/*-------------------Multithread/task---------------*/
 
 	/*--------------------------------------------------*/
 	/*                    Objet                         */
 	/*--------------------------------------------------*/
 
-	class Client{
-
-		//attribut
-		private $ncli;
-		private $nom;
-		private $adr;
-
-		//valeur par default necessaire
-		public function __construct($i=-1,$n="",$a="") {
-			$this->ncli = $i;
-			$this->nom = $n;
-			$this->adr = $a;
-		}
-
-		public function getIdcli() { return $this->ncli; }
-		public function getNom() { return $this->nom;}
-		public function getAdresse() { return $this->adr; }
-
-		public function __toString() {
-			$res = "idcli:".$this->ncli."\n";
-			$res = $res ."nom:".$this->nom."\n";
-			$res = $res ."adresse:".$this->adr."\n";
-			$res = $res ."<br/>";
-		 return $res;
-		}
-	}
-
 	/*-------------------heritage-----------------------*/
 
-	class Client2 extends Client
-	{
-		
-		function __construct(argument)
-		{
-
-		}
-	}
-
 	/*-------------------mot de language----------------*/
-
-		echo "string";
+	
+	__halt_compiler()	abstract	and	array()	as
+	break	callable	case	catch	class
+	clone	const	continue	declare	default
+	die()	do	echo	else	elseif
+	empty()	enddeclare	endfor	endforeach	endif
+	endswitch	endwhile	eval()	exit()	extends
+	final	finally	fn (as of PHP 7.4)	for	foreach
+	function	global	goto	if	implements
+	include	include_once	instanceof	insteadof	interface
+	isset()	list()	match (as of PHP 8.0)	namespace	new
+	or	print	private	protected	public
+	readonly (as of PHP 8.1.0) *	require	require_once	return	static
+	switch	throw	trait	try	unset()
+	use	var	while	xor	yield
 ?>
